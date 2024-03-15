e55d26b3a9446093857232fd0f363a658604535a
filou56 <filouwelle@gmail.com>
2024-03-15 14:54:31 +0100

    Windows build

---
+++ build.cmd
@@ -0,0 +1,7 @@
+copy ..\elzwelle_stopwatch.py .
+mkdir dist
+mkdir dist\elzwelle_stopwatch
+\opt\miniconda3\Scripts\pyinstaller.exe elzwelle_stopwatch.py
+copy \opt\miniconda3\Library\bin\libcrypto-3-x64.dll 	dist\elzwelle_stopwatch\_internal
+copy \opt\miniconda3\Library\bin\libssh2.dll 			dist\elzwelle_stopwatch\_internal
+copy \opt\miniconda3\Library\bin\libssl-3-x64.dll		dist\elzwelle_sheet\_internal
